373777ae32311cba6d11fe5d0bc077b504cefc00
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-01-30 15:36:22 +0700

    Signup route

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import { config } from "dotenv";
 config();
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { userRoutes } from "./routes/users";
 
 const app = fastify();
+app.register(cors, {origin: process.env.CLIENT_URRL});
+app.register(userRoutes)
 
-console.log("hi")
-
-app.listen({ port: parseInt(process.env.PORT!) })
+app.listen({ port: parseInt(process.env.PORT!) }, () => {
+    console.log("Server is running on Port " + process.env.PORT)
+})
