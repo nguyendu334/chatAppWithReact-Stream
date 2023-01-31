43ab45e7c459db704cd94d21424b01269ea02782
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-01-31 11:29:31 +0700

    Server Login Route

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import cors from "@fastify/cors";
 import { userRoutes } from "./routes/users";
 
 const app = fastify();
-app.register(cors, {origin: process.env.CLIENT_URRL});
+app.register(cors, {origin: process.env.CLIENT_URL});
 app.register(userRoutes)
 
 app.listen({ port: parseInt(process.env.PORT!) }, () => {
